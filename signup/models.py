8e1adf3fe471f43c2c57b2b529fa234887454fa0
PiromanV <vladimirvargasov@yandex.ru>
2024-12-08 02:15:54 +0300

    added notification settings and notification models

--- signup/models.py
+++ signup/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from films.models import Actor
+
+
+class NotificationSetting(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    notify_on_movie_update = models.BooleanField(default=False)
+    notify_on_actor_update = models.BooleanField(default=False)
+    notify_on_new_movie = models.BooleanField(default=False)
+    frequency = models.CharField(max_length=20, choices=[
+        ('immediate', 'Сразу после внесения данных'),
+        ('weekly', 'Раз в неделю'),
+        ('montly', 'Раз в месяц'),
+    ])
+    followed_actors = models.ManyToManyField(Actor, blank=True)
+
+
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextFiels()
+    created_at = models.DateTimeFIeld(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
