c96fbc062d54d5957edefd71f5a84ff5998f1ab5
PiromanV <vladimirvargasov@yandex.ru>
2024-12-08 17:35:17 +0300

    added schedule with celery

---
+++ celery.py
@@ -0,0 +1,8 @@
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'filmbase2024.settings')
+
+app = Celery('filmbase2024')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
